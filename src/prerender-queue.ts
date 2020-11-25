c0247b2578fdca76edfce25a0f36414c62248ca9
AkrISrn <shairkarine@gmail.com>
2020-11-25 22:47:21 +0800

    add prerender-queue.ts

---
+++ src/prerender-queue.ts
@@ -0,0 +1,28 @@
+import { Browser } from 'puppeteer-core';
+import { beginTo, loadPage, newPage, writeFile } from './utils';
+
+const loadedPaths: string[] = [];
+
+async function loadPages(browser: Browser, paths: string[]) {
+  const page = await newPage(browser);
+  const queue = [...paths];
+  let path = queue.shift();
+  while (path) {
+    loadedPaths.push(path);
+    const { html, paths } = await loadPage(page, path);
+    if (html) {
+      writeFile(path.replace(/\.md$/, '.html'), html);
+      paths.forEach(path => {
+        if (!loadedPaths.includes(path) && !queue.includes(path)) {
+          queue.push(path);
+        }
+      });
+    } else {
+      console.error('error:', path);
+    }
+    path = queue.shift();
+  }
+  await page.close();
+}
+
+(async () => await beginTo(loadPages))();
