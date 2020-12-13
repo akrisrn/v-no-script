833ddcaa303d557a3c61dcb12ce91a8a06466328
AkrISrn <shairkarine@gmail.com>
2020-12-13 23:15:47 +0800

    move getFiles to utils/index.ts

--- src/clean-html.ts
+++ src/clean-html.ts
@@ -1,22 +1,9 @@
 import fs from 'fs';
 import path from 'path';
-import { checkSitePath } from '@/utils';
-import { assetsDir, indexPath, sitePath } from '@/utils/vars';
+import { checkSitePath, getFiles } from '@/utils';
+import { indexPath, sitePath } from '@/utils/vars';
 
 checkSitePath();
-
-const excludeDirs = ['.git', '.github', assetsDir];
-
-const getFiles = async function* (dirPath: string): AsyncGenerator<string> {
-  for (const dirent of fs.readdirSync(dirPath, { withFileTypes: true })) {
-    const direntPath = path.join(dirPath, dirent.name);
-    if (!dirent.isDirectory()) {
-      yield direntPath;
-    } else if (!excludeDirs.includes(dirent.name)) {
-      yield* getFiles(direntPath);
-    }
-  }
-};
 
 const absoluteIndexPath = path.join(sitePath, indexPath);
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
-import { sitePath } from '@/utils/vars';
+import fs from 'fs';
+import path from 'path';
+import { assetsDir, sitePath } from '@/utils/vars';
 
 export function checkSitePath() {
   if (!sitePath) {
@@ -6,3 +8,16 @@ export function checkSitePath() {
     process.exit(1);
   }
 }
+
+const excludeDirs = ['.git', '.github', assetsDir];
+
+export const getFiles = async function* (dirPath: string): AsyncGenerator<string> {
+  for (const dirent of fs.readdirSync(dirPath, { withFileTypes: true })) {
+    const direntPath = path.join(dirPath, dirent.name);
+    if (!dirent.isDirectory()) {
+      yield direntPath;
+    } else if (!excludeDirs.includes(dirent.name)) {
+      yield* getFiles(direntPath);
+    }
+  }
+};
