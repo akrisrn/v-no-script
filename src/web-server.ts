4cb9e7d68df525889ea97b8783352032448c7fc3
AkrISrn <shairkarine@gmail.com>
2020-12-24 15:10:02 +0800

    add DISABLE_WS to .env

--- src/web-server.ts
+++ src/web-server.ts
@@ -5,71 +5,82 @@ import http from 'http';
 import WebSocket from 'ws';
 import watch from 'node-watch';
 import { checkSitePath, getRelative } from '@/utils';
-import { commonFile, excludeDirs, homePath, indexPath, localhost, port, publicPath, sitePath } from '@/utils/vars';
+import {
+  commonFile,
+  disableWS,
+  excludeDirs,
+  homePath,
+  indexPath,
+  localhost,
+  port,
+  publicPath,
+  sitePath,
+} from '@/utils/vars';
 
 checkSitePath();
 
-const absoluteIndexPath = path.join(sitePath, indexPath);
-const clientCodePath = path.join(__dirname, 'ws-client.js');
-
-function getIndexData() {
-  const indexData = fs.readFileSync(absoluteIndexPath).toString();
-  const clientCode = fs.readFileSync(clientCodePath).toString()
-    .replace(/(ws:\/\/localhost:)3000/, `$1${port}`)
-    .replace(/'\/common\.md'/, commonFile ? `'${commonFile}'` : '');
-  return indexData.replace(/(<\/body>)/, `<script>${clientCode}</script>$1`);
-}
-
-let indexData = getIndexData();
-
 const app = express();
-app.get(homePath, (request, response) => response.send(indexData));
-app.use(publicPath, express.static(sitePath));
-
 const server = http.createServer(app);
-server.listen(port, 'localhost', () => {
-  console.log(`Listening at ${localhost}${homePath}`);
-});
 
-const wss = new WebSocket.Server({ server });
-wss.on('connection', () => {
-  console.log(`Connected clients: ${wss.clients.size}`);
-});
+if (!disableWS) {
+  const wss = new WebSocket.Server({ server });
+  wss.on('connection', () => {
+    console.log(`Connected clients: ${wss.clients.size}`);
+  });
 
-function broadcast(data: string) {
-  wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
+  const broadcast = (data: string) => {
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  };
+
+  const absoluteIndexPath = path.join(sitePath, indexPath);
+  const clientCodePath = path.join(__dirname, 'ws-client.js');
+
+  const getIndexData = () => {
+    const indexData = fs.readFileSync(absoluteIndexPath).toString();
+    const clientCode = fs.readFileSync(clientCodePath).toString()
+      .replace(/(ws:\/\/localhost:)3000/, `$1${port}`)
+      .replace(/'\/common\.md'/, commonFile ? `'${commonFile}'` : '');
+    return indexData.replace(/(<\/body>)/, `<script>${clientCode}</script>$1`);
+  };
+
+  let indexData = getIndexData();
+
+  enum EventType {
+    refresh,
+    reload
+  }
+
+  const createResponse = (type: EventType, data: any = null) => JSON.stringify({ type, data });
+
+  watch([absoluteIndexPath, clientCodePath], () => {
+    indexData = getIndexData();
+    broadcast(createResponse(EventType.refresh));
+  });
+
+  const excludeRegExp = new RegExp(`[/\\\\](${excludeDirs.join('|')})[/\\\\]`);
+
+  watch(sitePath, {
+    recursive: true,
+    filter: (file, skip) => {
+      if (excludeRegExp.test(file)) {
+        return skip;
+      }
+      return /\.md$/.test(file);
+    },
+  }, (eventType, filePath) => {
+    if (filePath) {
+      broadcast(createResponse(EventType.reload, `/${getRelative(filePath)}`));
     }
   });
+
+  app.get(homePath, (request, response) => response.send(indexData));
 }
+app.use(publicPath, express.static(sitePath));
 
-enum EventType {
-  refresh,
-  reload
-}
-
-function createResponse(type: EventType, data: any = null) {
-  return JSON.stringify({ type, data });
-}
-
-watch([absoluteIndexPath, clientCodePath], () => {
-  indexData = getIndexData();
-  broadcast(createResponse(EventType.refresh));
-});
-
-const excludeRegExp = new RegExp(`[/\\\\](${excludeDirs.join('|')})[/\\\\]`);
-
-watch(sitePath, {
-  recursive: true,
-  filter: (file, skip) => {
-    if (excludeRegExp.test(file)) {
-      return skip;
-    }
-    return /\.md$/.test(file);
-  },
-}, (eventType, filePath) => {
-  if (filePath) {
-    broadcast(createResponse(EventType.reload, `/${getRelative(filePath)}`));
-  }
+server.listen(port, 'localhost', () => {
+  console.log(`Listening at ${localhost}${homePath}`);
 });
