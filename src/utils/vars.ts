cf1f61f41bf1308428986709391b3f1102a9d2ab
AkrISrn <shairkarine@gmail.com>
2020-12-31 17:03:54 +0800

    add escapeRE to vars.ts

--- src/utils/vars.ts
+++ src/utils/vars.ts
@@ -6,6 +6,12 @@ function getList(value?: string) {
   return value?.split(',').map(item => item.trim()).filter(item => item) ?? [];
 }
 
+const REGEXP_ESCAPE_RE = /[.?*+^$[\]\\(){}|-]/g;
+
+function escapeRE(str: string) {
+  return str.replace(REGEXP_ESCAPE_RE, '\\$&');
+}
+
 export const sitePath = process.env.SITE_PATH || '';
 export const outDir = process.env.OUT_DIR || sitePath || 'out';
 export const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
@@ -16,7 +22,7 @@ export const indexPath = process.env.INDEX_PATH || '-/index.html';
 export const cdnUrl = process.env.CDN_URL || '';
 export const indexFile = process.env.INDEX_FILE || '/index.md';
 export const commonFile = process.env.COMMON_FILE || '/common.md';
-export const excludeDirs = getList(process.env.EXCLUDE_DIRS);
+export const excludeDirs = getList(process.env.EXCLUDE_DIRS).map(escapeRE);
 export const excludeUsername = getList(process.env.EXCLUDE_USERNAME);
 export const addDeployTime = !!process.env.ADD_DEPLOY_TIME;
 export const useTimestamp = !!process.env.USE_TIMESTAMP;
